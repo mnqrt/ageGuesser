c2ef321b517ae9720805bd8e93e486d83d5aa6c2
Adrian <adrian.hamzah15@gmail.com>
2023-02-08 00:30:51 +0700

    .

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+alert(`Saya bisa menebak umur anda sekarang!!!!! 😱😱😨😨😨😱😱😨😨😨😱😱😨😨😨 REAL`);
 umur=+prompt("Berapa umur anda sekarang??");
 alert(`umur anda adalah ${umur} 😁🙏`);
 alert(`Karena saya benar, tolong follow @adrianhamzah2 😊🤭`);
